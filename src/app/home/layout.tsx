3941ee5b0dae5838f34a68832d1338feae2c8ee9
Maria Esther Joca <me.jocateixeira2015@gmail.com>
2025-09-06 12:57:38 +0000

    gemini corrije agoraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -112,9 +112,11 @@ function MainSidebar() {
             </SidebarMenuItem>
             <SidebarMenuItem>
                 <SidebarMenuButton onClick={() => router.push('/home/minha-colecao')} tooltip="Ver sua coleção de filhotes">
+                  <>
                     <FolderHeart />
                     <span>Minha Coleção</span>
                     {ownedPets.length > 0 && <Badge className="ml-auto flex h-6 w-6 items-center justify-center p-0">{ownedPets.length}</Badge>}
+                  </>
                 </SidebarMenuButton>
             </SidebarMenuItem>
             <SidebarMenuItem>
